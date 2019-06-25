980fc24034348b4cca812456093b2a72d875c83b
Samuel Veloso <samuelveloso42@gmail.com>
2019-06-25 12:54:06 -0300

    added to accept decimal numbers

---
+++ index.js
@@ -0,0 +1,51 @@
+let code = `x = 2
+x + 4`;
+
+const dicionario = [
+  {type: "operator", words: ["+", "-", "/", "="]},
+  {type: "int", words: ["0-9"]},
+  {type: "variable", words: ["a-z"]},
+];
+
+const operators = ["+", "-", "*", "/", "="];
+
+const createTokensLexicon = string => {
+  const tokens = string.split(/[; \s]/gi);
+  return tokens;
+}
+
+const typingAndVerifyTokensLexicon2 = tokens => { 
+  return tokens.map(token => {
+    let obj = { "token": token};
+   
+    dicionario.forEach(classes => {
+      classes.words.forEach( word_pattern => {
+        if(token.match(new RegExp("^["+word_pattern+"]+$", "gi"))){
+          obj["type"] =  classes.type;
+        }
+      });
+    });
+    if(typeof obj["type"] == "undefined"){
+      obj["type"] = "error";
+    }
+    return obj;
+  });
+}
+const typingAndVerifyTokensLexicon = tokens => { 
+  return tokens.map(token => {
+    let obj = { "token": token};
+    
+    if(operators.indexOf(token) != -1){
+      obj["type"] = "operator";
+    }
+    if(typeof obj["type"] == "undefined"){
+      obj["type"] = "error";
+    }
+    return obj;
+  });
+}
+const tokens = createTokensLexicon(code);
+
+// const tokens_typed = typingAndVerifyTokensLexicon(tokens);
+const tokens_typed2 = typingAndVerifyTokensLexicon2(tokens);
+console.log(tokens_typed2);
